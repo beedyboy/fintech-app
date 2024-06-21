61e27f2156e6aa5dfda3b63ab50170230b41da83
Bolade Akinniyi <boladebode@gmail.com>
2024-06-21 11:34:07 +0100

    chore: optimized transaction type and added additional fields

--- src/entities/transaction.entity.ts
+++ src/entities/transaction.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { User } from './user.entity';
+import { TransactionType } from 'src/enums/transaction.enum';
 
 @Entity()
 export class Transaction {
@@ -9,14 +10,23 @@ export class Transaction {
   @Column('decimal')
   amount: number;
 
-  @Column()
-  type: string;
+  @Column({
+    type: 'enum',
+    enum: TransactionType,
+  })
+  type: TransactionType;
 
   @Column()
   timestamp: string;
 
   @Column()
   paymentMethod: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column('decimal', { nullable: true })
+  balance: number;
 
   @ManyToOne(() => User, (user) => user.transactions)
   user: User;
