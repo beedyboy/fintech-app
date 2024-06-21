f25f893a1660b4e455a5789b4e40ca09e9f2184f
Bolade Akinniyi <boladebode@gmail.com>
2024-06-21 13:13:38 +0100

    chore: provisioned jwt to transaction module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -3,10 +3,13 @@ import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from 'src/entities/transaction.entity';
+import { AccessTokenStrategy } from 'src/guards/accessToken.strategy';
+import { JwtGuard } from 'src/guards/jwt.guards';
+import { User } from '../entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
-  providers: [TransactionService],
+  imports: [TypeOrmModule.forFeature([User, Transaction])],
+  providers: [TransactionService, JwtGuard, AccessTokenStrategy],
   controllers: [TransactionController],
 })
 export class TransactionModule {}
